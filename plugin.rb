3de2dd2cca34e33056a14f0c3da5c7fa0f88af94
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-06-14 21:16:51 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -2,4 +2,4 @@
 # about: You can automatically sell membership in particular user groups.
 # version: 1.0.0
 # authors: Dmitry Fedyuk
-# url: http://discourse.pro/t/35
+# url: https://discourse.pro/t/35
