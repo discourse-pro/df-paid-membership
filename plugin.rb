0ce8547a4228dc2e653c07603a2a35ceae609c38
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2015-07-10 00:25:55 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -5,6 +5,16 @@
 # url: https://discourse.pro/t/35
 gem 'attr_required', '1.0.0'
 gem 'paypal-express', '0.8.1', {require_name: 'paypal'}
+#require 'action_dispatch/middleware/debug_exceptions'
+gem 'airbrake', '4.3.0'
+=begin
+Airbrake.configure do |config|
+  config.api_key = 'c07658a7417f795847b2280bc2fd7a79'
+  config.host    = 'log.dmitry-fedyuk.com'
+  config.port    = 80
+  config.secure  = config.port == 443
+end
+=end
 require 'json'
 register_asset 'stylesheets/main.scss'
 after_initialize do
@@ -19,6 +29,7 @@ after_initialize do
 		#skip_before_filter :check_xhr
 		def index
 			begin
+				puts '!!!!!!!!!!!!!!!TEST!!!!!!!!!!!!!!!!!!!!!!!!'
 				plans = JSON.parse(SiteSetting.send '«Paid_Membership»_Plans')
 			rescue JSON::ParserError => e
 				plans = []
@@ -48,8 +59,7 @@ after_initialize do
 			puts params['user']
 			puts price
 			puts currency
-			userId = params['user']
-			user = User.find_by(id: userId)
+			user = User.find_by(id: params['user'])
 			Paypal.sandbox!
 			paypal_options = {
 				no_shipping: true, # if you want to disable shipping information
@@ -66,13 +76,13 @@ after_initialize do
 				" User: #{user.username}." +
 				" Period: #{tier['period']} #{tier['periodUnits']}."
 			puts description
-			paymentId = "#{userId}::#{planId}::#{tierId}::#{Time.now.strftime("%Y-%m-%d-%H-%M")}"
+			paymentId = "#{user.id}::#{planId}::#{tierId}::#{Time.now.strftime("%Y-%m-%d-%H-%M")}"
 			payment_request = Paypal::Payment::Request.new(
 				:currency_code => currency,
 				:description => description,
 				:quantity => 1,
 				:amount => price,
-				:notify_url => 'https://discourse.pro/plans/ipn',
+				:notify_url => "#{Discourse.base_url}/plans/ipn",
 				:invoice_number => paymentId,
 				:custom_fields => {
 					#CARTBORDERCOLOR: "C00000",
@@ -81,20 +91,34 @@ after_initialize do
 			)
 			response = request.setup(
 				payment_request,
-				'https://discourse.pro/plans',
-				'https://discourse.pro/plans',
+				# после успешной оплаты
+				# покупатель будет перенаправлен на свою личную страницу
+				"#{Discourse.base_url}/users/#{user.username}",
+				# в случае неупеха оплаты
+				# покупатель будет перенаправлен обратно на страницу с тарифными планами
+				"#{Discourse.base_url}/plans",
 				paypal_options
 			)
 			puts response.redirect_uri
 			render json: { redirect_uri: response.redirect_uri }
 		end
 		def ipn
+			Paypal::IPN.verify!(request.raw_post)
+		end
+		def test
+			puts '!!!!!!!!!!!!!!!!!test_mail!!!!!!!!!!!!!!!!!!!!!!!!'
+			#require_dependency 'application_controller'
+			require 'mailers/test_mailer'
+			message = TestMailer.send_test('dfediuk@gmail.com')
+			Email::Sender.new('тест', :test_message).send
+			puts '!!!!!!!!!!!!!!!Mail sent!!!!!!!!!!!!!!!!'
 		end
 	end
 	PaidMembership::Engine.routes.draw do
 		get '/' => 'index#index'
 		get '/buy' => 'index#buy'
 		get '/ipn' => 'index#ipn'
+		get '/test' => 'index#test'
 	end
 	Discourse::Application.routes.append do
 		mount ::PaidMembership::Engine, at: '/plans'
