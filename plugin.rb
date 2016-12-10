b2c5bb2100c45fcd3df13b0c24f2583832500821
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-10 03:27:27 +0300

    no message

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: df-paid-membership
 # about: You can automatically sell membership in particular user groups.
-# version: 3.1.0
+# version: 3.1.1
 # authors: Dmitry Fedyuk
 # url: https://discourse.pro/t/35
 # https://github.com/nov/paypal-express/wiki
