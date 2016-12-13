ea07cc4f7362796f9933088543f999973bbc99d5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-12-13 08:36:12 +0300

    3.3.0

--- app/controllers/df/paid_membership/success_recurring_controller.rb
+++ app/controllers/df/paid_membership/success_recurring_controller.rb
@@ -26,27 +26,33 @@ module ::Df::PaidMembership class SuccessRecurringController < SuccessController
 	def subscription_request_params
 		return @subscription_request_params if defined? @subscription_request_params
 		@subscription_request_params = Paypal::Payment::Recurring.new(
-			# https://developer.paypal.com/docs/classic/api/merchant/CreateRecurringPaymentsProfile_API_Operation_NVP/#id09BNA100BE6__id7bd11dfe-c599-4af2-aea0-cdea481bad4b
-			# SUBSCRIBERNAME
-			:name => user.name.empty? ? user.username : user.name,
-			# PROFILESTARTDATE
-			:start_date => invoice.created_at,
-			# PROFILEREFERENCE
-			:reference => invoice.id,
-			# https://developer.paypal.com/docs/classic/api/merchant/CreateRecurringPaymentsProfile_API_Operation_NVP/#id09BN9L00JPN__id156e9ad4-5e4d-463a-98fa-b34e350a1886
-			# DESC
-			:description => invoice.description,
 			# https://developer.paypal.com/docs/classic/api/merchant/CreateRecurringPaymentsProfile_API_Operation_NVP/#id09BNA01I0E9__idd4198f0a-9b54-4cb2-90e9-2c7b4fdd0324
 			:billing => {
-				# BILLINGPERIOD
-				:period => invoice.paypal_billing_period,
-				# BILLINGFREQUENCY
-				:frequency => invoice.tier_period,
 				# AMT
 				:amount => invoice.price,
 				# CURRENCYCODE
-				:currency_code => invoice.currency
-			}
+				:currency_code => invoice.currency,
+				# BILLINGFREQUENCY
+				:frequency => invoice.tier_period,
+				# BILLINGPERIOD
+				:period => invoice.paypal_billing_period,
+				:trial => {
+					:period => :Day,
+					:frequency => trialPeriod,
+					:total_cycles => 1,
+					:amount => 0
+				}
+			},
+			# https://developer.paypal.com/docs/classic/api/merchant/CreateRecurringPaymentsProfile_API_Operation_NVP/#id09BN9L00JPN__id156e9ad4-5e4d-463a-98fa-b34e350a1886
+			# DESC
+			:description => invoice.description,
+			# https://developer.paypal.com/docs/classic/api/merchant/CreateRecurringPaymentsProfile_API_Operation_NVP/#id09BNA100BE6__id7bd11dfe-c599-4af2-aea0-cdea481bad4b
+			# SUBSCRIBERNAME
+			:name => user.name.empty? ? user.username : user.name,
+			# PROFILEREFERENCE
+			:reference => invoice.id,
+			# PROFILESTARTDATE
+			:start_date => invoice.created_at
 		)
 	end
 	def subscription_response
